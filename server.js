6082c16c4d7dd2bac334e2f34b55bc210d7257ce
Cindy Chen <cindyjan15@gmail.com>
2017-07-28 21:11:20 -0700

    help

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var express = require("express");
 var methodOverride = require("method-override");
 var bodyParser = require("body-parser");
 
-var port = process.env.PORT || 8080;
+var PORT = process.env.PORT || 3000;
 
 var app = express();
 
